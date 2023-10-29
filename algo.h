0a5a1cf7cda3396f3f547a297ce0f1b33ece78a3
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2023-10-29 16:32:02 -0300

    Adicionado todas as palavras chaves de C

--- algo.h
+++ algo.h
@@ -3,26 +3,62 @@
 
 #include <stdio.h>
 
-#define inicio(x) main(x)
-#define funcao int
-#define retorna return
+// Palavras-chaves especiais
 #define estrutura struct
+#define enumeracao enum
+#define uniao union
+#define definetipo typedef
+#define retorna return
+#define sair break
+#define continue continue
+#define padrao default
 
-#define new(n, type) (type *) malloc(n*sizeof(type)) 
-#define piso(x) ((int)(x))
+// tipos
+#define Cadeia char*
+#define Vetor int*
+#define Array int*
+#define Inteiro int
+#define Logico int
+#define No int
+#define Indice int
+#define Real float
+#define Vazio void
+#define Caractere char
+#define automatico auto
+#define duplo double
+#define curto short
+#define longo long
 
+// modificadores
+#define constante const
+#define externo extern
+#define registrador register
+#define comsinal signed
+#define semsinal unsigned
+#define estatico static
+#define volatil volatile
+
+// Repetição e seleção
 #define enquanto(x) while(x)
 #define para(x) for(x)
+#define faca do
+#define vapara goto
 #define se(x) if(x)
 #define senao else
-#define novalinha puts("")
-#define escreva printf
-typedef char *Cadeia;
-typedef int *Array;
-typedef int No;
-typedef int inteiro;
-typedef int Indice;
+#define escolha switch
+#define caso case
 
+// Funções
+#define new(n, type) (type *) malloc(n*sizeof(type)) 
+#define piso(x) ((int)(x))
+#define escreva printf
+#define inicio(x) main(x)
+#define tamanho(x) sizeof(x)
+
+// Outros
+#define ok 0
+#define verdadeiro 1
+#define falso 0
 
 
 #endif
