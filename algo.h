470cc8b8f091bdf161f6cd655b92b561a7231f1f
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2023-10-29 15:29:13 -0300

    Create algo.h
    Cria arquivo principal que contem todas as macros e typedef's

---
+++ algo.h
@@ -0,0 +1,28 @@
+#ifndef ALGORITMIZADOR
+#define ALGORITMIZADOR
+
+#include <stdio.h>
+
+#define inicio(x) main(x)
+#define funcao int
+#define retorna return
+#define estrutura struct
+
+#define new(n, type) (type *) malloc(n*sizeof(type)) 
+#define piso(x) ((int)(x))
+
+#define enquanto(x) while(x)
+#define para(x) for(x)
+#define se(x) if(x)
+#define senao else
+#define novalinha puts("")
+#define escreva printf
+typedef char *Cadeia;
+typedef int *Array;
+typedef int No;
+typedef int inteiro;
+typedef int Indice;
+
+
+
+#endif
